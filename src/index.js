7d33d8097b7ebdc17abd7a3e2246bf07687cb989
Abdullah-Mujtaba <ahmedmujtaba823@gmail.com>
2025-06-07 03:10:19 +0500

    connection to mongoAtlas

---
+++ src/index.js
@@ -0,0 +1,35 @@
+import dotenv from "dotenv" // we import this because we want the environment variables to be available to everyone as soon as possible 
+// import express from "express"
+import connectDB from "./db/index.js";
+dotenv.config({
+    path: './env'
+})
+
+//Error [ERR_UNSUPPORTED_DIR_IMPORT]: Directory import 'C:\backend_project\src\db' is not supported resolving ES modules imported from C:\backend_project\src\index.js
+//when this happens look at the import statements
+
+//***********READ THE ERRORS!! */
+
+
+connectDB()
+
+
+// const app = express()
+// ( async () => {
+//     try{
+
+//         await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+//         app.on("error", (error)=>
+//             {
+//                 console.log("Error", error);
+//                 throw error
+//             })        
+//             app.listen(process.env.Port,() => {
+//                 console.log(`App is listening on port ${process.env.PORT}`)
+//             })
+//     }catch(error)
+//     {
+//         console.error("ERROR: ", error)
+//         throw err
+//     }
+// })()
