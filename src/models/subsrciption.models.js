fc0f3bac3a5c61322e9c3285035883d743219e41
Abdullah-Mujtaba <ahmedmujtaba823@gmail.com>
2025-06-11 16:54:13 +0500

    added a subscription model for our app

---
+++ src/models/subsrciption.models.js
@@ -0,0 +1,25 @@
+import mongoose, {Schema} from "mongoose";
+
+
+
+const subscriptionSchema = new Schema({
+    subscriber:
+    {
+        type: Schema.Types.ObjectId, //subscriber who subscribes
+        ref: "User",
+    },
+
+    channel: 
+    {
+        type: Schema.Types.ObjectId, //channel owner
+        ref: "User",
+    }
+
+
+},{timestamps:true})
+
+
+
+
+
+export const Subscription = mongoose.model("Subscription", subscriptionSchema)
